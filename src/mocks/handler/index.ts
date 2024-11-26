8f2458c9ba0af6497101c6585299a8b5a8911e61
hjlee2778 <lhj2778@dongguk.edu>
2024-11-27 01:04:59 +0900

    Feat: 로그인으로 인증되지 않으면 루트로 리다이렉트 되도록 구현 #SCRUM-86

--- src/mocks/handler/index.ts
+++ src/mocks/handler/index.ts
@@ -2,10 +2,12 @@ import { signUpHandlers } from './signUpHandlers';
 import { postHandlers } from './postHandlers';
 import { userHandlers } from './userHandlers';
 import { loginHandlers } from './loginHandlers';
+import { intermediateHandlers } from './IntermediateHandlers';
 
 export const handlers = [
   ...signUpHandlers,
   ...postHandlers,
   ...userHandlers,
   ...loginHandlers,
+  ...intermediateHandlers,
 ];
