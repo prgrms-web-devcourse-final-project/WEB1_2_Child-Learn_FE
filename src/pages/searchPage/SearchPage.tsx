cd9044fe0f73e07c5757c881054860aa70d28bd1
hjlee2778 <lhj2778@dongguk.edu>
2024-12-02 11:25:09 +0900

    Feat: 검색 바 디자인 구현 #SCRUM-121

--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -7,7 +7,14 @@ const SearchPage = () => {
 
   return (
     <ContentContainer>
-      <SearchBar value={searchTerm} onChange={setSearchTerm} />
+      <PageTitle>전체 사용자 검색</PageTitle>
+      <SearchBarWrapper>
+        <SearchBar
+          value={searchTerm}
+          onChange={setSearchTerm}
+          placeholder="Search"
+        />
+      </SearchBarWrapper>
       <ResultsContainer>{/* 검색 결과 컴포넌트들 */}</ResultsContainer>
     </ContentContainer>
   );
@@ -17,6 +24,19 @@ export default SearchPage;
 
 const ContentContainer = styled.div`
   padding: 20px;
+`;
+
+const PageTitle = styled.h1`
+  font-size: 17px;
+  font-weight: 700;
+  margin-top: 5px;
+  margin-bottom: 16px;
+  color: #181818;
+  text-align: center;
+`;
+
+const SearchBarWrapper = styled.div`
+  margin-bottom: 24px;
 `;
 
 const ResultsContainer = styled.div`
